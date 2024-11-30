3514077edd6827c2f9fabaed6fabf3c1b56ef3a1
nachocarher <nachoch64@correo.ugr.es>
2024-11-30 21:06:37 +0100

    Create starRating component

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  const [rat, setRat] = useState(0);
+  return (
+    <div>
+      <StarRating maxRating={3} onSetRating={setRat} />
+      <p>The rating is {rat}</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      className="test"
+      defaultRating={2}
+    />
+    <Test />
   </React.StrictMode>
 );
