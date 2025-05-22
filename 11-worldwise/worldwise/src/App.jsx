f0a9d46ae971f5275f8808e1936d0de28d379575
nachocarher <nachoch64@correo.ugr.es>
2025-05-22 17:37:11 +0200

    add components and styles for City, Country, and User features

--- 11-worldwise/worldwise/src/App.jsx
+++ 11-worldwise/worldwise/src/App.jsx
@@ -4,14 +4,17 @@ import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="/login" element={<Login />} />
+
         <Route path="*" element={<PageNotFound />} />
         <Route path="/app" element={<AppLayout />} />
       </Routes>
